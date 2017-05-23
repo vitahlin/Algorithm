a0ef5f7fc62c1aa3e890900b73e7d60801d11a46
Vitah <linw1225@gmail.com>
2017-05-23 10:51:44 +0800

    数组实现栈功能类初始化

--- CLRS/Chapter10/stack_1.cpp
+++ CLRS/Chapter10/stack_1.cpp
@@ -1,11 +1,40 @@
+/*============================================================================
+* Author        : vitah
+* Mail          : linw1225@163.com
+* Created       : 2017-05-23 10:40
+* Last modified : 2017-05-23 10:40
+* Filename      : stack_1.cpp
+* Description   : 数组实现栈功能
+=============================================================================*/
 
 #include<iostream>
 using namespace std;
 
-
 template <class T>
 class Stack {
+  private:
+    T * stack_array;	// 栈空间指针
+    int capacity;	// 栈的容量
+    int top_index;	// 栈顶
+  public:
+    Stack(int size = 10);
+    ~Stack();
+
 };
+
+// 构造函数，初始化栈数组的大小
+template <class T>
+Stack::Stack(int size) {
+    capacity = size;
+    stack_array = new T[size];
+    top_index = 0;
+}
+
+// 析构函数，释放数组空间
+template <class T>
+Stack::~Stack() {
+    delete [] stack_array;
+}
 
 
 int main() {
