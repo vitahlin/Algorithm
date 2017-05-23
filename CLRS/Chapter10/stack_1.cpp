435e93081e46b557875f0e2a03f74f33698ba480
Vitah <linw1225@gmail.com>
2017-05-23 11:17:03 +0800

    实现栈相关函数

--- CLRS/Chapter10/stack_1.cpp
+++ CLRS/Chapter10/stack_1.cpp
@@ -20,6 +20,11 @@ class Stack {
     Stack(int size = 10);
     ~Stack();
 
+    bool isFull();
+    bool push(T elem);
+    T pop();
+    void printStack();
+
 };
 
 // 构造函数，初始化栈数组的大小
@@ -34,6 +39,45 @@ Stack::Stack(int size) {
 template <class T>
 Stack::~Stack() {
     delete [] stack_array;
+}
+
+// 判断栈是否已经满
+template <class T>
+boo Stack::isFull() {
+    return capacity == top_index ? true : false;
+}
+
+// 元素入栈
+template <class T>
+bool Stack::push(T elem) {
+    if(isFull()) {
+        return false;
+    }
+
+    stack_array[top_index++] = elem;
+    return true;
+}
+
+// 元素出栈
+template <class T>
+T Stack::pop() {
+    if(top_index <= 0) {
+        return NULL;
+    }
+
+    T elem = stack_array[top_index--];
+    return elem;
+}
+
+// 打印栈内容
+template <class T>
+void Stack::printStack() {
+    int stack_size = top_index;
+
+    cout << "栈顶 ————> 栈底" << endl;
+    for(int i = stack_size; i >= 0; i--) {
+        cout << stack_array[i] << " ";
+    }
 }
 
 
